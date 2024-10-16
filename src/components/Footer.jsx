7c2ff666ec45e597ff44526d975894ec43e09820
SAQUIB-KHAN90 <saquibkhan890098@gmail.com>
2024-10-16 09:02:44 +0530

    Final commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,54 @@
+// import { FaGithub, FaLinkedin, FaGlobe, FaFilePdf } from "react-icons/fa";
+
+// const Footer = () => {
+//   return (
+//     <footer className="bg-gray-800 text-white py-6">
+//       <div className="container mx-auto text-center">
+//         {/* Footer text */}
+//         <p className="text-lg font-bold mb-4">Made by Saquib Khan</p>
+
+//         {/* Icons */}
+//         <div className="flex justify-center space-x-6">
+//           {/* GitHub */}
+//           <a
+//             href="https://github.com/SAQUIB-KHAN90"
+//             target="_blank"
+//             rel="noopener noreferrer"
+//             className="w-10 h-10 bg-gray-700 rounded-full flex items-center justify-center hover:bg-gray-600 transition duration-300"
+//           >
+//             <FaGithub size={20} />
+//           </a>
+//           {/* LinkedIn */}
+//           <a
+//             href="https://www.linkedin.com/in/saquib-khan-sa890098/"
+//             target="_blank"
+//             rel="noopener noreferrer"
+//             className="w-10 h-10 bg-gray-700 rounded-full flex items-center justify-center hover:bg-gray-600 transition duration-300"
+//           >
+//             <FaLinkedin size={20} />
+//           </a>
+//           {/* Portfolio */}
+//           <a
+//             href="https://saquib-khan-portfolio.netlify.app/"
+//             target="_blank"
+//             rel="noopener noreferrer"
+//             className="w-10 h-10 bg-gray-700 rounded-full flex items-center justify-center hover:bg-gray-600 transition duration-300"
+//           >
+//             <FaGlobe size={20} />
+//           </a>
+//           {/* Resume */}
+//           <a
+//             href="https://storage.googleapis.com/resume-hosting/66d9ad40482cc.pdf"
+//             target="_blank"
+//             rel="noopener noreferrer"
+//             className="w-10 h-10 bg-gray-700 rounded-full flex items-center justify-center hover:bg-gray-600 transition duration-300"
+//           >
+//             <FaFilePdf size={20} />
+//           </a>
+//         </div>
+//       </div>
+//     </footer>
+//   );
+// };
+
+// export default Footer;
